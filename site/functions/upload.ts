779546eb4dd916991f75fd8a718432a8180344c8
Arjun Chib <arjunpatrickchib@gmail.com>
2023-12-16 11:56:40 -0500

    Trigger webhook on upload

--- site/functions/upload.ts
+++ site/functions/upload.ts
@@ -4,5 +4,8 @@ interface Env {
 
 export const onRequest: PagesFunction<Env> = async (context) => {
   await context.env.BUCKET.put('test.png', context.request.body);
+  await fetch('https://ollie.arjunchib.com/webhook', {
+    method: 'POST',
+  });
   return new Response('ok');
 };
