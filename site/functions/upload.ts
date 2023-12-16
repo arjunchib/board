b38444f9c28be4ceabd53d577d76148060af1505
Arjun Chib <arjunpatrickchib@gmail.com>
2023-12-16 12:39:06 -0500

    Tee body to upload multiple files

--- site/functions/upload.ts
+++ site/functions/upload.ts
@@ -3,10 +3,11 @@ interface Env {
 }
 
 export const onRequest: PagesFunction<Env> = async (context) => {
+  const [stream1, stream2] = context.request.body.tee();
   const filename = new Date().toISOString() + '.png';
   await Promise.all([
-    await context.env.BUCKET.put('latest.png', context.request.body),
-    await context.env.BUCKET.put(filename, context.request.body),
+    await context.env.BUCKET.put('latest.png', stream1),
+    await context.env.BUCKET.put(filename, stream2),
   ]);
   await fetch('https://ollie.arjunchib.com/webhook', {
     method: 'POST',
